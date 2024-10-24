34671a75dbf6268fc3be0240ec610a8261e955ae
pmbata <34096316-pmbata@users.noreply.replit.com>
2024-10-24 02:04:26 +0000

    Agent step completed: Add new GATT characteristic with UUID 0xbbbb

--- service_definitions.py
+++ service_definitions.py
@@ -11,6 +11,7 @@ class ServiceDefinitions:
     TEMPERATURE_CHAR_UUID = "00000001-1111-2222-3333-444444444444"
     HUMIDITY_CHAR_UUID = "00000002-1111-2222-3333-444444444444"
     STATUS_CHAR_UUID = "00000003-1111-2222-3333-444444444444"
+    NEW_CHAR_UUID = "0000BBBB-0000-1000-8000-00805F9B34FB"  # Standard base UUID format with 0xBBBB
 
 class CharacteristicProperties:
     """Define characteristic properties and permissions."""
@@ -30,5 +31,11 @@ class CharacteristicProperties:
     STATUS = {
         "uuid": ServiceDefinitions.STATUS_CHAR_UUID,
         "properties": ["read"],
+        "initial_value": bytearray([0x00])
+    }
+
+    NEW_CHARACTERISTIC = {
+        "uuid": ServiceDefinitions.NEW_CHAR_UUID,
+        "properties": ["read", "write", "notify"],
         "initial_value": bytearray([0x00])
     }
