c26d8e971ebefa1bbc752be3daf49d7eeefcdfde
parkermakani <pmbata@gmail.com>
2024-10-25 16:30:27 +0000

    claude 1

--- ble_server.py
+++ ble_server.py
@@ -7,9 +7,10 @@ import dbus.service
 import logging
 import sys
 import os
+from datetime import datetime
 from gi.repository import GLib
 
-# D-Bus Interface constants
+# Constants
 BLUEZ_SERVICE_NAME = 'org.bluez'
 GATT_MANAGER_IFACE = 'org.bluez.GattManager1'
 DBUS_OM_IFACE = 'org.freedesktop.DBus.ObjectManager'
@@ -18,26 +19,15 @@ GATT_SERVICE_IFACE = 'org.bluez.GattService1'
 GATT_CHRC_IFACE = 'org.bluez.GattCharacteristic1'
 LE_ADVERTISING_MANAGER_IFACE = 'org.bluez.LEAdvertisingManager1'
 
-def setup_logging():
-    log_dir = "/var/log/pigattserver"
-    if not os.path.exists(log_dir):
-        os.makedirs(log_dir)
-    log_file = os.path.join(log_dir, "pigattserver.log")
-    logger = logging.getLogger('ble_server')
-    logger.setLevel(logging.DEBUG)
-    file_handler = logging.FileHandler(log_file)
-    console_handler = logging.StreamHandler(sys.stdout)
-    log_format = logging.Formatter(
-        '%(asctime)s - %(name)s - [%(levelname)s] - %(message)s',
-        datefmt='%Y-%m-%d %H:%M:%S'
-    )
-    file_handler.setFormatter(log_format)
-    console_handler.setFormatter(log_format)
-    logger.addHandler(file_handler)
-    logger.addHandler(console_handler)
-    return logger
-
-logger = setup_logging()
+# Set up logging
+logger = logging.getLogger('ble_server')
+logger.setLevel(logging.DEBUG)
+handler = logging.StreamHandler()
+handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - [%(levelname)s] - %(message)s', 
+                            datefmt='%Y-%m-%d %H:%M:%S')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 class InvalidArgsException(dbus.exceptions.DBusException):
     _dbus_error_name = 'org.freedesktop.DBus.Error.InvalidArgs'
@@ -45,135 +35,185 @@ class InvalidArgsException(dbus.exceptions.DBusException):
 class NotSupportedException(dbus.exceptions.DBusException):
     _dbus_error_name = 'org.bluez.Error.NotSupported'
 
-class Advertisement(dbus.service.Object):
-    PATH_BASE = '/org/bluez/example/advertisement'
+class Characteristic:
+    """Base characteristic class"""
+    def __init__(self, uuid, flags=['read']):
+        self.uuid = uuid
+        self.flags = flags
+        self.path = None
+        self.service = None
+        self.value = []
 
-    def __init__(self, bus, index, advertising_type):
-        self.path = self.PATH_BASE + str(index)
-        self.bus = bus
-        self.ad_type = advertising_type
-        self.service_uuids = None
-        self.manufacturer_data = None
-        self.solicit_uuids = None
-        self.service_data = None
-        self.local_name = 'SITR Device'
-        dbus.service.Object.__init__(self, bus, self.path)
+    def get_path(self):
+        return dbus.ObjectPath(self.path)
 
     def get_properties(self):
-        properties = {
-            'Type': self.ad_type,
-            'LocalName': dbus.String(self.local_name)
+        return {
+            GATT_CHRC_IFACE: {
+                'Service': self.service.get_path(),
+                'UUID': self.uuid,
+                'Flags': self.flags,
+                'Value': self.value
+            }
         }
-        if self.service_uuids is not None:
-            properties['ServiceUUIDs'] = dbus.Array(self.service_uuids, signature='s')
-        if self.solicit_uuids is not None:
-            properties['SolicitUUIDs'] = dbus.Array(self.solicit_uuids, signature='s')
-        if self.manufacturer_data is not None:
-            properties['ManufacturerData'] = dbus.Dictionary(self.manufacturer_data, signature='qv')
-        if self.service_data is not None:
-            properties['ServiceData'] = dbus.Dictionary(self.service_data, signature='sv')
-        return {LE_ADVERTISING_MANAGER_IFACE: properties}
 
-    @dbus.service.method(DBUS_PROP_IFACE, in_signature='s', out_signature='a{sv}')
+class Service:
+    """Base service class"""
+    PATH_BASE = '/org/bluez/example/service'
+
+    def __init__(self, uuid, primary=True):
+        self.uuid = uuid
+        self.primary = primary
+        self.characteristics = []
+        self.path = None
+
+    def get_path(self):
+        return dbus.ObjectPath(self.path)
+
+    def get_properties(self):
+        return {
+            GATT_SERVICE_IFACE: {
+                'UUID': self.uuid,
+                'Primary': self.primary,
+                'Characteristics': dbus.Array(
+                    self.get_characteristic_paths(),
+                    signature='o')
+            }
+        }
+
+    def get_characteristic_paths(self):
+        return [char.get_path() for char in self.characteristics]
+
+    def add_characteristic(self, characteristic):
+        self.characteristics.append(characteristic)
+        characteristic.service = self
+
+class DBusCharacteristic(dbus.service.Object, Characteristic):
+    """D-Bus enabled characteristic"""
+    def __init__(self, bus, index, uuid, flags, service_path):
+        self.path = service_path + '/char' + str(index)
+        logger.debug(f'Initializing characteristic at path: {self.path}')
+
+        # Initialize both parent classes
+        dbus.service.Object.__init__(self, bus, self.path)
+        Characteristic.__init__(self, uuid, flags)
+
+    @dbus.service.method(DBUS_PROP_IFACE,
+                        in_signature='s',
+                        out_signature='a{sv}')
     def GetAll(self, interface):
-        if interface != LE_ADVERTISING_MANAGER_IFACE:
+        if interface != GATT_CHRC_IFACE:
             raise InvalidArgsException()
-        return self.get_properties()[LE_ADVERTISING_MANAGER_IFACE]
+        return self.get_properties()[GATT_CHRC_IFACE]
 
-#!/usr/bin/env python3
+    @dbus.service.method(GATT_CHRC_IFACE,
+                        in_signature='ay',
+                        out_signature='ay')
+    def ReadValue(self, options):
+        logger.debug('ReadValue called')
+        return self.value
 
-# ... (keep your existing imports and logging setup)
+    @dbus.service.method(GATT_CHRC_IFACE,
+                        in_signature='aya{sv}')
+    def WriteValue(self, value, options):
+        logger.debug(f'WriteValue called with: {bytes(value)}')
+        self.value = value
+
+class DBusService(dbus.service.Object, Service):
+    """D-Bus enabled service"""
+    def __init__(self, bus, index, uuid, primary=True):
+        self.path = self.PATH_BASE + str(index)
+        logger.debug(f'Initializing service at path: {self.path}')
+
+        # Initialize both parent classes
+        dbus.service.Object.__init__(self, bus, self.path)
+        Service.__init__(self, uuid, primary)
+
+    @dbus.service.method(DBUS_PROP_IFACE,
+                        in_signature='s',
+                        out_signature='a{sv}')
+    def GetAll(self, interface):
+        if interface != GATT_SERVICE_IFACE:
+            raise InvalidArgsException()
+        return self.get_properties()[GATT_SERVICE_IFACE]
+
+class SITRCharacteristic(DBusCharacteristic):
+    """SITR specific characteristic"""
+    def __init__(self, bus, index, service_path):
+        super().__init__(
+            bus, index,
+            '12345678-1234-5678-1234-56789abcdef1',
+            ['read', 'write'],
+            service_path)
+
+class SITRService(DBusService):
+    """SITR specific service"""
+    def __init__(self, bus, index):
+        super().__init__(bus, index, '12345678-1234-5678-1234-56789abcdef0', True)
+        self.add_characteristic(SITRCharacteristic(bus, 0, self.path))
 
 class Application(dbus.service.Object):
     def __init__(self, bus):
         self.path = '/org/bluez/example'
         self.services = []
-        self.bus = bus
         super().__init__(bus, self.path)
-        self._logger = logging.getLogger('ble_server')
 
     def get_path(self):
         return dbus.ObjectPath(self.path)
 
     def add_service(self, service):
-        self._logger.debug(f'Adding service: {service.get_path()}')
+        logger.debug(f'Adding service: {service.get_path()}')
         self.services.append(service)
-        # No need to reinitialize the service as it's already initialized
 
-    @dbus.service.method(DBUS_OM_IFACE, out_signature='a{oa{sa{sv}}}')
+    @dbus.service.method(DBUS_OM_IFACE,
+                        out_signature='a{oa{sa{sv}}}')
     def GetManagedObjects(self):
         response = {}
-        self._logger.debug('GetManagedObjects called')
-
         for service in self.services:
             response[service.get_path()] = service.get_properties()
-            chrcs = service.get_characteristics()
-            for chrc in chrcs:
-                response[chrc.get_path()] = chrc.get_properties()
-
-        self._logger.debug(f'GetManagedObjects response: {response}')
+            for char in service.characteristics:
+                response[char.get_path()] = char.get_properties()
         return response
 
-class Service(dbus.service.Object):
-    PATH_BASE = '/org/bluez/example/service'
-
-    def __init__(self, bus, index, uuid, primary):
-        self.path = self.PATH_BASE + str(index)
+class Advertisement(dbus.service.Object):
+    def __init__(self, bus, index, advertising_type):
+        self.path = f'/org/bluez/example/advertisement{index}'
         self.bus = bus
-        self.uuid = uuid
-        self.primary = primary
-        self.characteristics = []
-        self._logger = logging.getLogger('ble_server')
-        self._logger.debug(f'Service initialized: {self.path}')
+        self.ad_type = advertising_type
+        self.local_name = 'SITR Device'
         super().__init__(bus, self.path)
 
-    def add_characteristic(self, characteristic):
-        self._logger.debug(f'Adding characteristic: {characteristic.get_path()}')
-        self.characteristics.append(characteristic)
+    def get_path(self):
+        return dbus.ObjectPath(self.path)
 
-class Characteristic(dbus.service.Object):
-    def __init__(self, bus, index, uuid, flags, service):
-        self.path = service.path + '/char' + str(index)
-        self.bus = bus
-        self.uuid = uuid
-        self.service = service
-        self.flags = flags
-        self.value = []
-        self._logger = logging.getLogger('ble_server')
-        self._logger.debug(f'Characteristic initialized: {self.path}')
-        super().__init__(bus, self.path)
+    def get_properties(self):
+        properties = dict()
+        properties['Type'] = self.ad_type
+        if self.local_name:
+            properties['LocalName'] = dbus.String(self.local_name)
+        return {LE_ADVERTISING_MANAGER_IFACE: properties}
 
-class SITRService(Service):
-    SITR_UUID = '12345678-1234-5678-1234-56789abcdef0'
+    @dbus.service.method(DBUS_PROP_IFACE,
+                        in_signature='s',
+                        out_signature='a{sv}')
+    def GetAll(self, interface):
+        if interface != LE_ADVERTISING_MANAGER_IFACE:
+            raise InvalidArgsException()
+        return self.get_properties()[LE_ADVERTISING_MANAGER_IFACE]
 
-    def __init__(self, bus, index):
-        super().__init__(bus, index, self.SITR_UUID, True)
-        self.add_characteristic(SITRCharacteristic(bus, 0, self))
-
-class SITRCharacteristic(Characteristic):
-    SITR_CHARACTERISTIC_UUID = '12345678-1234-5678-1234-56789abcdef1'
-
-    def __init__(self, bus, index, service):
-        super().__init__(
-            bus, index,
-            self.SITR_CHARACTERISTIC_UUID,
-            ['read', 'write'],
-            service)
-
-def find_adapter(bus, adapter_pattern=None):
-    remote_om = dbus.Interface(bus.get_object(BLUEZ_SERVICE_NAME, '/'), DBUS_OM_IFACE)
+def find_adapter(bus):
+    remote_om = dbus.Interface(bus.get_object(BLUEZ_SERVICE_NAME, '/'),
+                              DBUS_OM_IFACE)
     objects = remote_om.GetManagedObjects()
 
     for path, interfaces in objects.items():
-        if GATT_MANAGER_IFACE not in interfaces:
-            continue
-        if adapter_pattern is None or path.endswith(adapter_pattern):
+        if GATT_MANAGER_IFACE in interfaces:
             return bus.get_object(BLUEZ_SERVICE_NAME, path)
+
+    logger.error('Bluetooth adapter not found')
     raise Exception('Bluetooth adapter not found')
 
 def main():
-    logger = logging.getLogger('ble_server')
-
     try:
         logger.debug('Setting up DBus main loop.')
         dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
@@ -183,9 +223,6 @@ def main():
 
         logger.debug('Finding BLE adapter...')
         adapter = find_adapter(bus)
-        if not adapter:
-            logger.error('BLE adapter not found')
-            sys.exit(1)
         logger.debug('BLE adapter found.')
 
         logger.debug('Creating service manager and advertisement manager interfaces.')
@@ -195,8 +232,6 @@ def main():
         logger.debug('Creating advertisement and GATT application.')
         advertisement = Advertisement(bus, 0, 'peripheral')
         app = Application(bus)
-
-        # Create and add service
         app.add_service(SITRService(bus, 0))
 
         mainloop = GLib.MainLoop()
@@ -215,14 +250,13 @@ def main():
 
         logger.info('GATT server is running. Press Ctrl+C to stop.')
 
-        try:
-            mainloop.run()
-        except KeyboardInterrupt:
-            logger.info('Shutting down GATT server')
-            ad_manager.UnregisterAdvertisement(advertisement.get_path())
-            service_manager.UnregisterApplication(app.get_path())
-            logger.info('Server stopped')
+        mainloop.run()
 
+    except KeyboardInterrupt:
+        logger.info('Shutting down...')
+        ad_manager.UnregisterAdvertisement(advertisement.get_path())
+        logger.info('Advertisement unregistered')
+        sys.exit(0)
     except Exception as e:
         logger.error(f'Fatal error: {str(e)}')
         logger.debug('Exception details:', exc_info=True)
