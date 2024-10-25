13680774a97f87d2c97678acb1db57b90d8727b1
parkermakani <pmbata@gmail.com>
2024-10-25 08:23:07 +0000

    claude6

--- ble_server.py
+++ ble_server.py
@@ -7,78 +7,56 @@ import dbus.service
 import logging
 import sys
 import os
+import subprocess
 from datetime import datetime
 from gi.repository import GLib
 
-# Set up logging configuration
 def setup_logging():
-    # Create logs directory if it doesn't exist
     log_dir = "/var/log/pigattserver"
     if not os.path.exists(log_dir):
         os.makedirs(log_dir)
-
-    # Set up file for logging
     log_file = os.path.join(log_dir, "pigattserver.log")
-
-    # Create a logger
     logger = logging.getLogger('ble_server')
     logger.setLevel(logging.DEBUG)
-
-    # Create handlers
     file_handler = logging.FileHandler(log_file)
     console_handler = logging.StreamHandler(sys.stdout)
-
-    # Create formatters and add it to handlers
     log_format = logging.Formatter(
         '%(asctime)s - %(name)s - [%(levelname)s] - %(message)s',
         datefmt='%Y-%m-%d %H:%M:%S'
     )
     file_handler.setFormatter(log_format)
     console_handler.setFormatter(log_format)
-
-    # Add handlers to the logger
     logger.addHandler(file_handler)
     logger.addHandler(console_handler)
-
     return logger
 
-class GATTCharacteristic(dbus.service.Object):
-    def __init__(self, bus, index, uuid, flags, service):
-        self.path = service.path + '/char' + str(index)
-        self.bus = bus
-        self.uuid = uuid
-        self.service = service
-        self.flags = flags
-        dbus.service.Object.__init__(self, bus, self.path)
+def check_bluetooth_service():
+    """Check if bluetooth service is running and configured properly"""
+    try:
+        # Check bluetooth service status
+        status = subprocess.run(['systemctl', 'is-active', 'bluetooth'], 
+                              capture_output=True, text=True)
+        if status.stdout.strip() != 'active':
+            subprocess.run(['systemctl', 'start', 'bluetooth'])
+            subprocess.run(['systemctl', 'enable', 'bluetooth'])
 
-    def get_properties(self):
-        return {
-            'org.bluez.GattCharacteristic1': {
-                'Service': self.service.get_path(),
-                'UUID': self.uuid,
-                'Flags': self.flags,
-            }
-        }
+        # Ensure bluetooth is powered on
+        subprocess.run(['bluetoothctl', 'power', 'on'])
+        subprocess.run(['bluetoothctl', 'discoverable', 'on'])
+        return True
+    except Exception as e:
+        return False
 
-    @dbus.service.method('org.freedesktop.DBus.Properties',
-                        in_signature='s', out_signature='a{sv}')
-    def GetAll(self, interface):
-        if interface != 'org.bluez.GattCharacteristic1':
-            raise dbus.exceptions.DBusException(
-                'org.bluez.Error.InvalidArgs',
-                'Interface {} not supported'.format(interface))
-        return self.get_properties()['org.bluez.GattCharacteristic1']
+class SITRService(dbus.service.Object):
+    """SITR GATT Service"""
 
-class GATTService(dbus.service.Object):
-    PATH_BASE = '/org/bluez/example/service'
-
-    def __init__(self, bus, index, uuid, primary):
-        self.path = self.PATH_BASE + str(index)
+    def __init__(self, bus, index, uuid, primary='true'):
+        self.path = f'/org/bluez/example/service{index}'
         self.bus = bus
         self.uuid = uuid
         self.primary = primary
         self.characteristics = []
-        dbus.service.Object.__init__(self, bus, self.path)
+        super().__init__(bus, self.path)
 
     def get_properties(self):
         return {
@@ -103,17 +81,64 @@ class GATTService(dbus.service.Object):
             result.append(chrc.get_path())
         return result
 
-    @dbus.service.method('org.freedesktop.DBus.Properties',
+    @dbus.service.method(dbus.PROPERTIES_IFACE,
                         in_signature='s',
                         out_signature='a{sv}')
     def GetAll(self, interface):
         if interface != 'org.bluez.GattService1':
             raise dbus.exceptions.DBusException(
                 'org.bluez.Error.InvalidArgs',
-                'Interface {} not supported'.format(interface))
+                f'Interface {interface} not supported')
         return self.get_properties()['org.bluez.GattService1']
 
+class SITRCharacteristic(dbus.service.Object):
+    """SITR GATT Characteristic"""
+
+    def __init__(self, bus, index, uuid, flags, service):
+        self.path = service.path + '/char' + str(index)
+        self.bus = bus
+        self.uuid = uuid
+        self.service = service
+        self.flags = flags
+        self.value = []
+        super().__init__(bus, self.path)
+
+    def get_properties(self):
+        return {
+            'org.bluez.GattCharacteristic1': {
+                'Service': self.service.get_path(),
+                'UUID': self.uuid,
+                'Flags': self.flags,
+            }
+        }
+
+    def get_path(self):
+        return dbus.ObjectPath(self.path)
+
+    @dbus.service.method(dbus.PROPERTIES_IFACE,
+                        in_signature='s',
+                        out_signature='a{sv}')
+    def GetAll(self, interface):
+        if interface != 'org.bluez.GattCharacteristic1':
+            raise dbus.exceptions.DBusException(
+                'org.bluez.Error.InvalidArgs',
+                f'Interface {interface} not supported')
+        return self.get_properties()['org.bluez.GattCharacteristic1']
+
+    @dbus.service.method('org.bluez.GattCharacteristic1',
+                        in_signature='a{sv}',
+                        out_signature='ay')
+    def ReadValue(self, options):
+        return self.value
+
+    @dbus.service.method('org.bluez.GattCharacteristic1',
+                        in_signature='aya{sv}')
+    def WriteValue(self, value, options):
+        self.value = value
+
 class BLEGATTServer(dbus.service.Object):
+    """Main GATT Server class"""
+
     def __init__(self, bus, adapter_name, logger):
         self.path = '/org/bluez/example'
         self.logger = logger
@@ -122,20 +147,25 @@ class BLEGATTServer(dbus.service.Object):
         self.bus = bus
         super().__init__(bus, self.path)
 
+        if not check_bluetooth_service():
+            raise Exception("Failed to initialize Bluetooth service")
+
         self._setup_dbus()
-        self._add_service()
+        self._add_services()
 
-    def _add_service(self):
-        """Add a test service"""
-        service = GATTService(self.bus, 0, 
-                            '12345678-1234-5678-1234-56789abcdef0', True)
-        self.services.append(service)
+    def _add_services(self):
+        """Add SITR services"""
+        # Main SITR service
+        sitr_service = SITRService(self.bus, 0, 
+                                 '12345678-1234-5678-1234-56789abcdef0')
+        self.services.append(sitr_service)
 
-        # Add a test characteristic
-        char = GATTCharacteristic(self.bus, 0,
+        # Add test characteristic
+        char = SITRCharacteristic(self.bus, 0,
                                 '12345678-1234-5678-1234-56789abcdef1',
-                                ['read', 'write'], service)
-        service.add_characteristic(char)
+                                ['read', 'write'], 
+                                sitr_service)
+        sitr_service.add_characteristic(char)
 
     def get_path(self):
         return dbus.ObjectPath(self.path)
@@ -144,15 +174,23 @@ class BLEGATTServer(dbus.service.Object):
         try:
             self.logger.debug("Setting up D-Bus interfaces")
             adapter_path = f'/org/bluez/{self.adapter_name}'
-            adapter = self.bus.get_object('org.bluez', adapter_path)
 
+            # Get adapter object
+            adapter = self.bus.get_object('org.bluez', adapter_path)
+            self.logger.debug(f"Got adapter object at {adapter_path}")
+
+            # Get adapter properties
             adapter_props = dbus.Interface(adapter, 'org.freedesktop.DBus.Properties')
 
+            # Power on adapter if needed
             if not adapter_props.Get('org.bluez.Adapter1', 'Powered'):
+                self.logger.debug("Powering on Bluetooth adapter")
                 adapter_props.Set('org.bluez.Adapter1', 'Powered', dbus.Boolean(1))
 
+            # Get required interfaces
             self.ad_manager = dbus.Interface(adapter, 'org.bluez.LEAdvertisingManager1')
             self.service_manager = dbus.Interface(adapter, 'org.bluez.GattManager1')
+            self.logger.debug("D-Bus interfaces setup completed")
 
         except dbus.exceptions.DBusException as e:
             self.logger.error(f"D-Bus setup failed: {str(e)}")
@@ -172,7 +210,9 @@ class BLEGATTServer(dbus.service.Object):
     def start(self):
         try:
             self.logger.info("Starting GATT server")
-            self.service_manager.RegisterApplication(self.get_path(), {})
+            # Set longer timeout for registration
+            self.service_manager.RegisterApplication(self.get_path(), {}, 
+                                                  timeout=30000)
             self.logger.info("GATT application registered successfully")
 
         except dbus.exceptions.DBusException as e:
